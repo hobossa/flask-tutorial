2f41315a76bf8a9861434d3454ee9a51698dbe05
hobossa <hoboss@gmail.com>
2021-09-22 21:09:38 -0700

    Done: test

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -15,7 +15,7 @@ def create_app(test_config=None):
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
-        app.config.frommapping(test_config)
+        app.config.update(test_config)
 
     # ensure the instance folder exists
     try:
@@ -26,7 +26,7 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello')
     def hello():
-        return 'Hello, World!!'
+        return 'Hello, World!'
 
     from . import db
     db.init_app(app)
